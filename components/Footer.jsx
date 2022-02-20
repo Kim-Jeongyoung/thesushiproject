a1612f95a990061d1e4789421b4d124868cb4266
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-02-20 08:54:45 -0800

    ADD BASIC GRID

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,110 +14,49 @@ import MuiNextLink from '@components/MuiNextLink';
 import { Container, Grid, Typography } from '@mui/material';
 
 //Add logo in the middle 2/19
-import Logo from '../public/rollinlogowhite.svg';
+import FooterLogo from '../public/footer_rollinlogo_white.svg';
 import IconButton from '@mui/material/IconButton';
 import NextLink from 'next/link';
 import MuiLink from '@mui/material/Link';
+// import Image from 'next/image';
+// import Image from 'material-ui-image';
 
 const Footer = () => {
   return (
-    <Box
-      component="footer"
-      sx={{
-        bgcolor: 'primary.main',
-        textDecoration: 'none',
-        color: 'common.white',
-        px: 1,
-      }}
-    >
-      <Container maxWidth="lg">
-        <Box textAlign="center">LOGO IMAGE</Box>
-        {/* centered logo */}
-        {/* <Box sx={{ align: 'center' }}>
-          <Container
-            maxWidth="lg"
-            sx={{
-              display: `flex`,
-              justifyContent: `center`,
-              alignItems: `center`,
-            }}
-          >
-            <IconButton edge="start" aria-label="home">
-              <MuiNextLink activeClassName="active" href="/">
-                <Logo />
-              </MuiNextLink>
-            </IconButton>
-          </Container>
-        </Box> */}
-
-        {/* 다시 코드 이해 필요 */}
-        <Grid
-          container
-          spacing={2}
-          direction="row"
-          justifyContent="space-between"
-          alignItmes="center"
-        >
-          <Grid item xs={12} sm={6}>
-            <Box>
-              {/* <Link href="/" color="inherit"> */}
-              CONTACT US
-              {/* </Link> */}
-            </Box>
-            <Box>
-              {/* <Link href="/" color="inherit"> */}
-              Email info@rollinsushi.com
-              {/* </Link> */}
-            </Box>
-            <Box>Phone 956-249-0167</Box>
+    <Box component="footer" sx={{ py: 5, bgcolor: '#40464d' }}>
+      <Container>
+        {/* Logo Grid */}
+        <Grid columns={3}>
+          <Box sx={{ align: 'center' }}>
+            <FooterLogo />
+          </Box>
+        </Grid>
+        {/* Second Grid with Contact and Follow on us */}
+        <Grid>
+          <Grid>
+            <Grid container>
+              <Grid item>
+                <Box>CONTACT US</Box>
+                <Box>Email info@rollinsushi.com</Box>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Box>FIND US ON</Box>
-            <Box component="span" sx={{ py: 1 }}>
-              {/* 아이콘 간격 줄이기 */}
-              <Stack
-                direction="row"
-                justifyContent="space-between"
-                spacing={1}
-                sx={{ mb: 5 }}
-              >
-                <MuiNextLink
-                  sx={{ textDecoration: 'none', color: 'common.white' }}
-                  href="https://YourFacebookLink/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Facebook fontSize="large" />
-                </MuiNextLink>
-                <MuiNextLink
-                  sx={{ textDecoration: 'none', color: 'common.white' }}
-                  href="https://YourInstagramLink/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Instagram fontSize="large" />
-                </MuiNextLink>
-                <MuiNextLink
-                  sx={{ textDecoration: 'none', color: 'common.white' }}
-                  href="https://YourTwitterLink/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Twitter fontSize="large" />
-                </MuiNextLink>
-              </Stack>
-            </Box>
+          {/* Follow on us Grid */}
+          <Grid>
+            <Grid container>
+              <Grid item>
+                <Box>Follow on Us</Box>
+                <Box>social icons</Box>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
-
-        <Typography align="center" color="common.white">
-          © 2022 Sushi Rollin'
-        </Typography>
+        <Grid>
+          <Typography align="center" color="common.white">
+            © 2022 Sushi Rollin'
+          </Typography>
+        </Grid>
       </Container>
-
-      {/* <Stack direction="row" justifyContent="center" spacing={4} sx={{ mb: 5 }}> */}
-
-      {/* </Stack> */}
     </Box>
   );
 };
@@ -160,3 +99,160 @@ const Footer = () => {
 // };
 
 export default Footer;
+
+//2/19 일 작업 한 코드
+
+{
+  /* <Box
+  component="footer"
+  sx={{
+    bgcolor: '#262626',
+    textDecoration: 'none',
+    color: 'common.white',
+    px: 1,
+  }}
+>
+  <Container maxWidth="lg">
+    <Grid>
+      <Box sx={{ align: 'center' }}>
+        <FooterLogo />
+        {/* <Image src="public/footer_rollinlogo_white.svg" /> */
+}
+// </Box>
+// </Grid>
+
+{
+  /* centered logo */
+}
+{
+  /* <Box sx={{ align: 'center' }}>
+          <Container
+            maxWidth="lg"
+            sx={{
+              display: `flex`,
+              justifyContent: `center`,
+              alignItems: `center`,
+            }}
+          >
+            <IconButton edge="start" aria-label="home">
+              <MuiNextLink activeClassName="active" href="/">
+                <Logo />
+              </MuiNextLink>
+            </IconButton>
+          </Container>
+        </Box> */
+}
+
+{
+  /* 다시 코드 이해 필요 */
+}
+//   <Grid
+//     container
+//     spacing={2}
+//     direction="row"
+//     justifyContent="space-between"
+//     alignItmes="center"
+//   >
+//     <Grid item xs={12} sm={6}>
+//       <Box>
+//         {/* <Link href="/" color="inherit"> */}
+//         CONTACT US
+//         {/* </Link> */}
+//       </Box>
+//       <Box>
+//         {/* <Link href="/" color="inherit"> */}
+//         Email info@rollinsushi.com
+//         {/* </Link> */}
+//       </Box>
+//       <Box>Phone 956-249-0167</Box>
+//     </Grid>
+//     <Grid item xs={12} sm={6} sx={{ width: 300 }}>
+//       <Box>FIND US ON</Box>
+//       <Box component="span" sx={{ py: 1 }}>
+//         {/* 아이콘 간격 줄이기 */}
+//         <Stack
+//           direction="row"
+//           justifyContent="space-between"
+//           spacing={1}
+//           sx={{ mb: 5 }}
+//           px
+//         >
+//           <MuiNextLink
+//             sx={{ textDecoration: 'none', color: 'common.white' }}
+//             href="https://YourFacebookLink/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <Facebook fontSize="large" />
+//           </MuiNextLink>
+//           <MuiNextLink
+//             sx={{ textDecoration: 'none', color: 'common.white' }}
+//             href="https://YourInstagramLink/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <Instagram fontSize="large" />
+//           </MuiNextLink>
+//           <MuiNextLink
+//             sx={{ textDecoration: 'none', color: 'common.white' }}
+//             href="https://YourTwitterLink/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <Twitter fontSize="large" />
+//           </MuiNextLink>
+//         </Stack>
+//       </Box>
+//     </Grid>
+//   </Grid>
+
+//   <Typography align="center" color="common.white">
+//     © 2022 Sushi Rollin'
+//   </Typography>
+// </Container>
+
+{
+  /* <Stack direction="row" justifyContent="center" spacing={4} sx={{ mb: 5 }}> */
+}
+
+{
+  /* </Stack> */
+}
+// </Box>; */}
+
+// 2/20 시작 코드
+// <Box component="footer" sx={{ py: 5, bgcolor: '#40464d' }}>
+//       <Container>
+//         {/* Logo Grid */}
+//         <Grid columns={3}>
+//           <Box sx={{ align: 'center' }}>
+//             <FooterLogo />
+//           </Box>
+//         </Grid>
+//         {/* Second Grid with Contact and Follow on us */}
+//         <Grid>
+//           <Grid>
+//             <Grid container>
+//               <Grid item>
+//                 <Box>CONTACT US</Box>
+//                 <Box>Email info@rollinsushi.com</Box>
+//               </Grid>
+//             </Grid>
+//           </Grid>
+//           {/* Follow on us Grid */}
+//           <Grid>
+//             <Grid container>
+//               <Grid item>
+//                 <Box>Follow on Us</Box>
+//                 <Box>social icons</Box>
+//               </Grid>
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//         <Grid>
+//           <Typography align="center" color="common.white">
+//             © 2022 Sushi Rollin'
+//           </Typography>
+//         </Grid>
+//       </Container>
+//     </Box>
